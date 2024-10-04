4595a1626b5ac913c38b8a18f4b51ce5cbc7a387
excilyano <agahnim13@gmail.com>
2024-10-04 15:37:53 +0200

    demo room OK

--- mod6demoroom/build.gradle.kts
+++ mod6demoroom/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -51,11 +51,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.room.runtime)
-    annotationProcessor(libs.androidx.room.compiler)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
-    testImplementation(libs.androidx.room.testing)
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    testImplementation("androidx.room:room-testing:$room_version")
 
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
