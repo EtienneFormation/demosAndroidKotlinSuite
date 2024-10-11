d813fa7cc21a4a42475d77f3b9677ab0d094a625
riderofzion <quentinrouet@hotmail.fr>
2024-10-11 14:50:26 +0200

    correction Formulaire connexion

--- filrouge/src/main/java/fr/eni/filrouge/ui/view/page/SignInPage.kt
+++ filrouge/src/main/java/fr/eni/filrouge/ui/view/page/SignInPage.kt
@@ -1,8 +1,9 @@
 package fr.eni.filrouge.ui.view.page
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -21,29 +22,52 @@ fun SignInPage(navigationToArticles : () -> Unit,
     var stateEmail by remember { mutableStateOf("") }
     var stateEmailError by remember { mutableStateOf("") }
     var statePwd by remember { mutableStateOf("") }
-    Column {
+    var statePwdError by remember { mutableStateOf(true) }
+    LazyColumn(Modifier
+        .fillMaxHeight()
+        .padding(16.dp)) {
         //TODO Faire la page de connexion
         //Champ Email (TextField)
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp, vertical = 8.dp),
-            value = stateEmail,
-            isError = stateEmailError.isNotEmpty(),
-            onValueChange = {
-                stateEmail = it
-                if(!stateEmail.contains("@")){
-                    stateEmailError = "L'email doit contenir @"
-                }else{stateEmailError = "" }
-            },
-            label = { Text("Email") }
-        )
-        if(stateEmailError.isNotEmpty())
-            Text("Erreur $stateEmailError",color= MaterialTheme.colorScheme.error)
-        //Text error is erreur présente
-        //Champ Password avec PasswordVisualisation
-        //Text error si erreur présente
-        Modifier.weight(1f)
-        ElevatedButton(onClick = navigationToArticles) {
-            Text("Se Connecter")
+        item {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                value = stateEmail,
+                isError = stateEmailError.isNotEmpty(),
+                onValueChange = {
+                    stateEmail = it
+                    if (!stateEmail.contains("@")) {
+                        stateEmailError = "L'email doit contenir @"
+                    } else {
+                        stateEmailError = ""
+                    }
+                },
+                label = { Text("Email") }
+            )
+            if (stateEmailError.isNotEmpty())
+                Text("Erreur $stateEmailError", color = MaterialTheme.colorScheme.error)
+            //Text error is erreur présente
+            //Champ Password avec PasswordVisualisation
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                value = statePwd,
+                isError = statePwdError,
+                onValueChange = {
+                    statePwd = it
+                    statePwdError = statePwd.isEmpty()
+                },
+                label = { Text("Mot de passe") }
+            )
+            if (statePwdError)
+                Text("Le mot de passe est vide!", color = MaterialTheme.colorScheme.error)
+            //Text error si erreur présente
+            //Spacer(Modifier.weight(1f))
+            ElevatedButton(
+                onClick = navigationToArticles,
+                Modifier.fillMaxWidth(),
+                enabled = !statePwdError && stateEmailError.isEmpty()
+            ) {
+                Text("Se Connecter")
+            }
         }
     }
 }
